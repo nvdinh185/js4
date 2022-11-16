eaccf15f29e22e2f819bf6c2f6074c6c61e9ee9f
nvdinh185 <nvdinh0766777123@gmail.com>
2022-11-16 14:52:19 +0700

    y

--- validation.js
+++ validation.js
@@ -2,29 +2,19 @@ function Validator(options) {
     var formElement = document.querySelector(options.form);
     if (formElement) {
 
-        var selectorRules = {};
         options.rules.forEach(function (rule) {
-
-            if (Array.isArray(selectorRules[rule.selector])) {
-                selectorRules[rule.selector].push(rule.test);
-            } else {
-                selectorRules[rule.selector] = [rule.test];
-            }
 
             var inputElement = formElement.querySelector(rule.selector);
             if (inputElement) {
                 inputElement.onblur = function () {
-                    var errorMsg;
-                    var rules = selectorRules[rule.selector];
-                    for (let i = 0; i < rules.length; i++) {
-                        errorMsg = rules[i](inputElement.value);
-                        if (errorMsg) break;
-                    }
                     var errorElement = inputElement.parentElement.querySelector(".form-message");
+                    var errorMsg = rule.test(inputElement.value);
                     if (errorMsg) {
                         errorElement.innerText = errorMsg;
+                        inputElement.parentElement.querySelector(".form-control").classList.add('invalid');
                     } else {
-                        errorElement.innerText = '';
+                        errorElement.innerText = "";
+                        inputElement.parentElement.querySelector(".form-control").classList.remove('invalid');
                     }
                 }
             }
@@ -40,26 +30,3 @@ Validator.isRequired = function (selector) {
         }
     }
 }
-
-Validator.isEmail = function (selector) {
-    return {
-        selector: selector,
-        test: function (value) {
-            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-            return regex.test(value) ? undefined : "Trường này phải là email";
-        }
-    }
-}
-
-// ========================================================================================
-
-// function Validator2(options) {
-//     console.log(options);
-// }
-
-// Validator2.myMethod = function () {
-//     console.log(123);
-//     return {
-//         a: 123
-//     };
-// }
